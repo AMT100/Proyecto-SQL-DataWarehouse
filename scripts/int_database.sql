9508ae3b6e923180e6b08e025b1c035c1970824b
AMT100 <alejandromigueltovar97@gmail.com>
2026-03-12 10:17:48 +0100

    Create int_database.sql

---
+++ scripts/int_database.sql
@@ -0,0 +1,36 @@
+/* 
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+Script para crear una nueva base de datos llamada 'DataWarehouse', después de comprobar que efectivamente existe y de eliminarla para volverla a crear si es el caso.
+Creación de esquemas bronze, silver y gold.
+
+ESTE SCRIPT BORRARÁ LA BASE DE DATOS 'DATAWAREHOUSE' POR COMPLETO SI ESTA EXISTE, HACER COPIA DE SEGURIDAD ANTES DE EJECUTARLO
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+*/
+
+USE master;
+GO
+
+--Drop DataWarehouse y volverlo a crear
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK INMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+--Crear el DataWarehouse
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+--Crear esquemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
